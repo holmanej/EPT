300ba2213ab8131f44960ca2949fc34e56aa6328
holmanej <holmanej@gmail.com>
2020-05-08 12:51:41 -0400

    Added data files

--- EVE Production Tool/Program.cs
+++ EVE Production Tool/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Remoting.Messaging;
 using System.Windows.Forms;
 using static EVE_Production_Tool.MarketBrowser;
 
@@ -16,10 +18,30 @@ namespace EVE_Production_Tool
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
+            Console.WriteLine(FindAssets("assets"));
+
             Form1 EPT_Form = new Form1();
             EPT_Form.Controls.Add(new MarketBrowser());
 
             Application.Run(EPT_Form);
+        }
+
+        static bool FindAssets(string dirName)
+        {
+            while (!File.Exists("EVE Production Tool.sln"))
+            {
+                Console.WriteLine(Directory.GetCurrentDirectory());
+                Directory.SetCurrentDirectory("..");
+            }
+            if (Directory.Exists(dirName))
+            {
+                Directory.SetCurrentDirectory(dirName);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
